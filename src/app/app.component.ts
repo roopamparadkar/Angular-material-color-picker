f4dd154a1416d8e1e9bd6012b3c439db857f0366
Blackhawk <roopamparadkar@gmail.com>
2020-06-07 13:18:06 +0530

    add color picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Material-Color-Picker';
+
+  selectedEventColor = {
+    color: 'Peacock',
+    class: "peacock",
+    background: "#039be5",
+    textColor: "#000000"
+  };
+  paletteColor: any;
+  timeEnabled = false;
+  allDayEvent = true;
+  repeatEvent = false;
+  repeatDays = [];
+
+  colors = [
+    {
+      color: 'Tomato',
+      class: "tomato",
+      background: "#d50000",
+      textColor: "#ffffff"
+    },
+    {
+      color: 'Tangerine',
+      class: "tangerine",
+      background: "#f4511e",
+      textColor: "#ffffff"
+    },
+    {
+      color: 'Sage',
+      class: "sage",
+      background: "#33b679",
+      textColor: "#ffffff"
+    },
+    {
+      color: 'Blueberry',
+      class: "blueberry",
+      background: "#3f51b5",
+      textColor: "#000000"
+    },
+    {
+      color: 'Peacock',
+      class: "peacock",
+      background: "#039be5",
+      textColor: "#000000"
+    },
+    {
+      color: 'Grape',
+      class: "grape",
+      background: "#8e24aa",
+      textColor: "#ffffff"
+    },
+    {
+      color: 'Graphite',
+      class: "graphite",
+      background: "#616161",
+      textColor: "#ffffff"
+    },
+    {
+      color: 'Basil',
+      class: "basil",
+      background: "#0b8043",
+      textColor: "#ffffff"
+    }
+  ];
+  paletteColorChange() {
+    this.paletteColor = this.selectedEventColor.background;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTooltipModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
